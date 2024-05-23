af1622a6a4b1c048355a262ce5e1377c4d867c3f
TheFFPS <shmelev.lev@gmail.com>
2024-05-23 22:00:13 +0300

    Add some loot tables, recipes, lang, models, blocks, items

--- src/main/java/org/ffpsss/forcefieldreborn/ForcefieldReborn.java
+++ src/main/java/org/ffpsss/forcefieldreborn/ForcefieldReborn.java
@@ -4,14 +4,18 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 import org.ffpsss.forcefieldreborn.block.capacitor.*;
 import org.ffpsss.forcefieldreborn.block.extension.*;
 import org.ffpsss.forcefieldreborn.common.util.BlockUtil;
 import org.ffpsss.forcefieldreborn.common.util.ItemUtil;
+import org.ffpsss.forcefieldreborn.item.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,29 +28,47 @@ public class ForcefieldReborn implements ModInitializer {
 		LOGGER.info("Hello Fabric world!");
 		
 		ITEM_GROUP = FabricItemGroupBuilder.create(new Identifier("forcefieldreborn", "forcefieldreborn_group")).icon(() -> new ItemStack(
-			BlockUtil.REGISTERED_BLOCK_ITEMS.get("basic_field_generator")
+			ItemUtil.REGISTERED_ITEMS.get("field_projector_unit")
 		)).build();
 
 		BlockUtil.register(
-			new BasicCapacitor(FabricBlockSettings.of(Material.METAL).strength(4.0f).requiresTool()), 
+			new BasicCapacitor(FabricBlockSettings.of(Material.METAL).strength(6.0f).requiresTool()), 
 			"basic_capacitor", 
 			true
 		);
 		BlockUtil.register(
-			new AdvancedCapacitor(FabricBlockSettings.of(Material.METAL).strength(4.0f).requiresTool()), 
+			new AdvancedCapacitor(FabricBlockSettings.of(Material.METAL).strength(6.0f).requiresTool()), 
 			"advanced_capacitor", 
 			true
 		);
 		BlockUtil.register(
-			new HighAdvancedCapacitor(FabricBlockSettings.of(Material.METAL).strength(4.0f).requiresTool()), 
+			new HighAdvancedCapacitor(FabricBlockSettings.of(Material.METAL).strength(6.0f).requiresTool()), 
 			"high_advanced_capacitor", 
 			true
 		);
 
 		BlockUtil.register(
-			new InventoryDropExtension(FabricBlockSettings.of(Material.METAL).strength(4.0f).requiresTool()), 
+			new InventoryDropExtension(FabricBlockSettings.of(Material.METAL).strength(6.0f).requiresTool()), 
 			"inventory_drop_extension", 
 			true
+		);
+
+		BlockUtil.register(
+			new OreBlock(
+				FabricBlockSettings.of(Material.METAL).strength(8.0f).requiresTool(),
+				UniformIntProvider.create(5, 9)
+			), 
+			"forcicium_ore", 
+			true
+		);
+
+		ItemUtil.register(
+			new FieldProjectorUnit(new Item.Settings().maxCount(64).group(ForcefieldReborn.ITEM_GROUP)),
+			"field_projector_unit"
+		);
+		ItemUtil.register(
+			new Forcicium(new Item.Settings().maxCount(64).group(ForcefieldReborn.ITEM_GROUP)),
+			"forcicium"
 		);
 	}
 }
---
+++ src/main/java/org/ffpsss/forcefieldreborn/item/Forcicium.java
@@ -0,0 +1,9 @@
+package org.ffpsss.forcefieldreborn.item;
+
+import net.minecraft.item.Item;
+
+public class Forcicium extends Item {
+    public Forcicium(Settings settings) {
+        super(settings);
+    }
+}
